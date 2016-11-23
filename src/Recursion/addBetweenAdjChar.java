a178a97cb2bb4dab18a38a22afbc201854ecc078
jennifernghinguyen <jennifernghi@yahoo.com>
2016-11-22 21:46:25 -0800

    add a char between adjacent character recursively

--- src/Recursion/addBetweenAdjChar.java
+++ src/Recursion/addBetweenAdjChar.java
@@ -4,7 +4,7 @@ public class addBetweenAdjChar {
 	
 	public String pair(String str, char c){
 		while(str.length()>=2){
-			if (str.charAt(0)==str.charAt(1) && str.length()>=2){
+			if (str.charAt(0)==str.charAt(1)){
 			return String.valueOf(str.charAt(0)) + String.valueOf(c) + pair(str.substring(1), c);
 		}else{
 			return String.valueOf(str.charAt(0))+ pair(str.substring(1), c);
@@ -15,7 +15,7 @@ public class addBetweenAdjChar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		addBetweenAdjChar p = new addBetweenAdjChar();
-		System.out.println(p.pair("aaa", '!'));
+		System.out.println(p.pair("aaaa", '!'));
 		System.out.println(p.pair("hello", '!'));
 		System.out.println(p.pair("xxyy", '!'));
 	}
