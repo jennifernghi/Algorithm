74a9241066040a09e5496e8f35cb9376a938a3eb
jennifernghinguyen <jennifernghi@yahoo.com>
2016-11-23 21:27:30 -0800

    countOnlyOneChildNode

--- src/Recursion/Node.java
+++ src/Recursion/Node.java
@@ -19,7 +19,7 @@ public class Node {
 	public static Node createNode(int number) {
 		return new Node(number);
 	}
-	
+
 	public static void main(String[] args) {
 		// root level 0
 		Node A = Node.createNode(60);
@@ -29,40 +29,41 @@ public class Node {
 		// Level 2
 		Node D = Node.createNode(10);
 		Node E = Node.createNode(30);
-		/*Node F = Node.createNode(70);
-		Node G = Node.createNode(90);
-		// Level 3
-		Node H = Node.createNode(65);
-		Node I = Node.createNode(75);
-		Node J = Node.createNode(95);*/
+		 Node F = Node.createNode(70); //Node G = Node.createNode(90); // Level
+		/* 3 Node H = Node.createNode(65); Node I = Node.createNode(75); Node J
+		 * = Node.createNode(95);
+		 */
 
 		// connect Level 0 and 1
 		A.left = B;
 		A.right = C;
 		// connect level 1 and level 2
 		B.left = D;
-		//B.right = E;
+		// B.right = E;
 		C.left = E;
-		//C.right = G;
+		E.left = F;
 		// connect level 2 and level 3
-		/*D.left = H;
-		F.right = I;
-		G.right = J;*/
-		
+		/*
+		 * D.left = H; F.right = I; G.right = J;
+		 */
+
 		System.out.println(Node.countOnlyOneChildNode(A));
 	}
-	
+
 	public static int countOnlyOneChildNode(Node root) {
 
-
-	     if(root == null || (root.left == null && root.right == null))
-	         return 0;
-	     else 
-	    {
-	    	 if(root.left == null || root.right == null)
-	    		 return 1 + countOnlyOneChildNode(root.left) + countOnlyOneChildNode(root.right);
-	    	 else
-	    		 return countOnlyOneChildNode(root.left) + countOnlyOneChildNode(root.right);
-	    	 }
-	    }
+		if (root == null || (root.left == null && root.right == null))
+			return 0;
+		else {
+			if (root.left == null) {
+				return 1 + countOnlyOneChildNode(root.right);
+			}
+			else if (root.right == null) {
+				return 1 + countOnlyOneChildNode(root.left);
+			} 
+			else {
+				return countOnlyOneChildNode(root.left) + countOnlyOneChildNode(root.right);
+			}
+		}
+	}
 }
